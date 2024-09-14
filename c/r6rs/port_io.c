46377f3597b43744b2477f7e9b9d2fb7ebb97847
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2024-09-14 10:06:32 +0100

    step for port io

--- c/r6rs/port_io.c
+++ c/r6rs/port_io.c
@@ -19,6 +19,13 @@
 #include "io.h"
 #include "port_io.h"
 
+
+typedef struct _io_result {
+    FILE* handle;
+    long error_code;
+    char* error_message;
+} io_result;
+
 /* our header to export and define interface */
 
 /*
@@ -30,15 +37,38 @@ file-options buffer-mode)
 (open-file-input-port filename procedure
 file-options buffer-mode maybe-transcoder) */
 
+static io_result make_io_result(FILE* handle, long err_return) {
+        io_result* result = malloc(sizeof(io_result))
+        result->handle = handle;
+        result->error_code = err_return;
+        result->error_message = ps_error_string(err_return);
+        return result;
+}
+
+static s48_ref_t make_port_result(s48_call_t call, FILE* handle, long err_return) {
+     io_result* result = make_io_result(handle, err_return);
+     return s48_make_local_ref(call, (s48_value) result);
+}
+
+
 
 static s48_ref_t r6rs_open_file_input_port(s48_call_t call, s48_ref_t path,
             s48_ref_t file_options) {
+    long length = s48_string_utf_8_length_2(call, sch_in);
+    buffer = (char*)s48_make_local_buf(call, length);
+    s48_copy_string_to_utf_8_2(call, sch_in, buffer);
     FILE* handle = ps_open_input_output_file(char *name, long *status);
+    return make_port_result(call, handle, 0L);
 }
 
 static s48_ref_t r6rs_open_file_output_port(s48_call_t call, s48_ref_t path,
              s48_ref_t file_options) {
+             // read write vector in read / wrie funs do it today
+    long length = s48_string_utf_8_length_2(call, sch_in);
+    buffer = (char*)s48_make_local_buf(call, length);
+    s48_copy_string_to_utf_8_2(call, sch_in, buffer);
     FILE* handle = ps_open_output_file(char *name, long *status);
+    return make_port_result(call, handle, 0L);
 }
 
 static s48_ref_t r6rs_open_file_input_output_port(s48_call_t call, s48_ref_t path,
@@ -47,15 +77,23 @@ static s48_ref_t r6rs_open_file_input_output_port(s48_call_t call, s48_ref_t pat
     buffer = (char*)s48_make_local_buf(call, length);
     s48_copy_string_to_utf_8_2(call, sch_in, buffer);
     FILE* handle = ps_open_input_output_file(char *name, long *status);
+    return make_port_result(call, handle, 0L);
 }
 
 
+
+static s48_ref_t r6rs_close_file(s48_ref_t file_handle) {
+    FILE* handle =  (FILE*)s48_deref(file_handle)
+    long result = ps_close_fd(handle);
+    return make_port_result(handle, result);
+}
 
 void s48_init_port_io(void)
 {
   S48_EXPORT_FUNCTION(r6rs_open_file_output_port);
   S48_EXPORT_FUNCTION(r6rs_open_file_input_port);
   S48_EXPORT_FUNCTION(r6rs_open_file_input_output_port);
+  S48_EXPORT_FUNCTION(r6rs_close_file);
 }
 
 
--- scheme/r6rs/port-io.scm
+++ scheme/r6rs/port-io.scm
@@ -2,11 +2,6 @@
 
 ; Authors: Harald Glab-Plhak
 
-;; the default file options
-(define *default-file-options* )
-
-
-
 (define os-enc (get-os-string-encoding))
 
 ;; define the file options via enums like defined for r6rs
@@ -22,29 +17,35 @@
   (lambda ()
     ))
 
+;; the default file options
+(define *default-file-options* )
+(define *default-buffer-mode* )
+(define *default-eol-style* )
+(define *default-transcoder* )
 
 ;;static s48_ref_t r6rs_open_file_input_port(s48_call_t call, s48_ref_t path,
 ;            s48_ref_t file_options, s48_ref_t buffer_mode,
 ;            s48_ref_t maybe_transcoder) {
 
 (define open-file-input-port
-  (opt-lambda (filename ((file-options *default-file-options*)))
-    (external-r6rs-open-file-input-port filename os-enc )))
+  (opt-lambda (file-path ((file-options *default-file-options*)
+                           (file-buffer-mode *default-file-options*)))
+    (external-r6rs-open-file-input-port file-path os-enc )))
 
 ;;static s48_ref_t r6rs_open_file_output_port(s48_call_t call, s48_ref_t path,
 ;             s48_ref_t file_options, s48_ref_t buffer_mode,
 ;             s48_ref_t maybe_transcoder)
 
 (define open-file-output-port
-  (opt-lambda (filename ((file-options *default-file-options*)))
-    (external-r6rs-open-file-output-port filename os-enc)))
+  (opt-lambda (file-path ((file-options *default-file-options*)))
+    (external-r6rs-open-file-output-port file-path os-enc)))
 
 ;;static s48_ref_t r6rs_open_file_input_output_port(s48_call_t call, s48_ref_t path,
 ;                                                         s48_ref_t file_options, s48_ref_t buffer_mode,
 ;                                                         s48_ref_t maybe_transcoder) {
 (define open-file-input-output-port
-  (opt-lambda (filename ((file-options *default-file-options*)))
-    (external-r6rs-open-file-input-output-port filename os-enc)))
+  (opt-lambda (file-path ((file-options *default-file-options*)))
+    (external-r6rs-open-file-input-output-port file-path os-enc)))
 
 
 ;; the imports for externals
